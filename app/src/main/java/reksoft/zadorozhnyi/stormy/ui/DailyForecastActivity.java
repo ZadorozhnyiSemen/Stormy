635e5e8025e5455a07b68beeae784924e9d3084d
Zadorozhnyi <Zadorozhnyi@reksoft.ru>
2017-01-30 16:46:57 +0300

    add array adapter

--- app/src/main/java/reksoft/zadorozhnyi/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/reksoft/zadorozhnyi/stormy/ui/DailyForecastActivity.java
@@ -3,6 +3,7 @@ package reksoft.zadorozhnyi.stormy.ui;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import reksoft.zadorozhnyi.stormy.R;
 
@@ -14,5 +15,8 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
     }
 }
